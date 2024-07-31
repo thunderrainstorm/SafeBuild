7f54d357377c005def2d925382013b4be1cf167a
Sambita Mondal <156224126+thunderrainstorm@users.noreply.github.com>
2024-07-31 19:18:44 +0530

    Update start_flask_app.bat

--- start_flask_app.bat
+++ start_flask_app.bat
@@ -1,4 +1,5 @@
 @echo off
 cd C:\Users\sambita\webapp
 call venv\Scripts\activate
-waitress-serve --listen=0.0.0.0:80 app:app
+echo %date% %time% > flask_app.log
+waitress-serve --listen=0.0.0.0:80 app:app >> flask_app.log 2>&1
